9aa4a31d3b83d514996557c1fa483a70ac4ef5e9
Alexander Shchetkov [Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2022-03-26 20:25:48 +0300

    Неточность
    Неточность

--- lab5/14_Program.fs
+++ lab5/14_Program.fs
@@ -13,10 +13,7 @@ let PDON n func intinial =
             let ndiv = div - 1
             SPDON n func nintinial ndiv 
     SPDON n func intinial n
-
-
-
-
+    
 [<EntryPoint>]
 let main argv =
     printf "MOD: %A" (PDON 78 (fun x y -> x+y)0) 
