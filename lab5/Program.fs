c3fb6f5f173c56dbe02bf9592859c104689dca72
Alexander Shchetkov [Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2022-03-26 19:14:27 +0300

    Add files via upload

---
+++ lab5/Program.fs
@@ -0,0 +1,23 @@
+﻿//---LOL---
+//14
+
+open System
+
+let PDON n func intinial = 
+    let rec SPDON n func intinial div = 
+        if div = 0 then intinial 
+        else 
+            let nintinial = 
+                if n%div = 0 then func intinial div 
+                else intinial 
+            let ndiv = div - 1
+            SPDON n func nintinial ndiv 
+    SPDON n func intinial n
+
+
+
+
+[<EntryPoint>]
+let main argv =
+    printf "MOD: %A" (PDON 78 (fun x y -> x-y)0)//разност делителей 
+    0 // return an integer exit code
