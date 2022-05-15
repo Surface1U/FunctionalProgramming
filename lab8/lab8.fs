fbcb8af61a336be5bcd976b208032131f33834ff
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2022-05-15 18:49:39 +0300

    Update lab8.fs

--- lab8/lab8.fs
+++ lab8/lab8.fs
@@ -1,18 +1,69 @@
-abstract member Print: unit -> unit
+open System.Text.RegularExpressions
+open System.Diagnostics
+
+type IPrint = interface
+    abstract member Print: unit -> unit
     end
 
 
 type VehiclePassport(i:int, name:string, model:string, category:string, yearOfManufacture:int, enginePower:double) = 
-
+        
         member this.ID
-@@ -59,7 +60,33 @@ type VehiclePassport(i:int, name:string, model:string, category:string, yearOfMa
+            with get() = i
+            and set(value:int) = 
+                if Regex.IsMatch (System.Convert.ToString(value), @"[0-9]")
+                then this.ID <- value
+                else System.Console.WriteLine("Invalid input") 
+
+        member this.Name
+            with get() = name
+            and set(value:string) =
+                if Regex.IsMatch (value, @"[a-z]")
+                then this.Name <- value
+                else System.Console.WriteLine("Invalid input")
+
+        member this.Model
+            with get() = model
+            and set(value:string) = 
+                if Regex.IsMatch (value, @"[a-z]")
+                then  this.Model <- value
+                else System.Console.WriteLine("Invalid input") 
+            
+        member this.Category
+            with get() = category
+            and set(value:string) = 
+                if Regex.IsMatch (System.Convert.ToString(value), @"[a-z]")
+                then this.Category <- value
+                else System.Console.WriteLine("Invalid input") 
+
+        member this.YearOfManufacture
+            with get() = yearOfManufacture
+            and set(value:int) =
+                if Regex.IsMatch (System.Convert.ToString(value), @"[0-9]")
+                then this.YearOfManufacture <- value
+                else System.Console.WriteLine("Invalid input") 
+
+        member this.EnginePower
+            with get() = enginePower
+            and set(value:float) = 
+                if Regex.IsMatch (System.Convert.ToString(value), @"[0-9]+.[0-9]+")
+                then this.EnginePower <- value
+                else System.Console.WriteLine("Invalid input")
+
+        override this.ToString() =            
+            sprintf "ID = %i\nName = %s\nModel = %s\nCategory = %s\nYearOfManufacture = %i\nEnginePower = %f\n"
+                this.ID this.Name this.Model this.Category this.YearOfManufacture this.EnginePower
+                                      
+        interface IPrint with
+            member this.Print() =
+                this.ToString()
                 |> printfn "%s\n"
         member this.Print() = (this :> IPrint).Print()
 
         interface System.IComparable<VehiclePassport> with
             member this.CompareTo other =
                 compare this.ID other.ID
-
+                
         interface System.IComparable with
             member this.CompareTo object = 
                 match object with
@@ -29,14 +80,3 @@ type VehiclePassport(i:int, name:string, model:string, category:string, yearOfMa
             | :? VehiclePassport as other -> (this :> System.IEquatable<_>).Equals other
             |_ -> false
         override this.GetHashCode () = hash this.ID
-
-let ForDemo =
-    let rand = System.Random()
-    let vehicles = List.init(10) (fun v -> VehiclePassport((rand.Next(100, 1000)), "unnamed", "unknown", "undefined", (rand.Next(2000, 2022)), 0.0001))
-    vehicles |> List.iter (fun v -> v.Print())  
-    //vehicles |> List.iter (fun v -> v.Print())  
-    //Equation by ID and YearOfManufacture
-    //Comparison by ID
-    printfn "%A" ((new VehiclePassport(1, "", "", "", 10, 0.0)).Equals(new VehiclePassport(2, "", "", "", 10, 0.4))) // false
-    printfn "%A" ((new VehiclePassport(1, "", "", "", 10, 0.0)).Equals(new VehiclePassport(1, "", "", "", 10, 0.4))) // true
-    printfn "%A" (compare (new VehiclePassport(10, "", "", "", 20, 0.0)) (new VehiclePassport(4, "", "", "", 40, 0.4)))// 1
