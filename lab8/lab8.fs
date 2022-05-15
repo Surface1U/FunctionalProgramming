b6b6e16e0d5e10fd7ecfd05c59ac8c679560a067
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2022-05-15 18:05:07 +0300

    Update lab8.fs

--- lab8/lab8.fs
+++ lab8/lab8.fs
@@ -1,38 +1,42 @@
-open System.Text.RegularExpressions
-
-type VehiclePassport(name:string, model:string, category:string, yearOfManufacture:int, enginePower:double) = 
-type IPrint = interface
-    abstract member Print: unit -> unit
+abstract member Print: unit -> unit
     end
+
 
 type VehiclePassport(i:int, name:string, model:string, category:string, yearOfManufacture:int, enginePower:double) = 
 
         member this.ID
-            with get() = i
-            and set(value:int) = 
-                if Regex.IsMatch (System.Convert.ToString(value), @"[0-9]")
-                then this.ID <- value
-                else System.Console.WriteLine("Invalid input") 
-
-        member this.Name
-            with get() = name
-            and set(value:string) =
-@@ -37,3 +48,18 @@ type VehiclePassport(name:string, model:string, category:string, yearOfManufactu
-                if Regex.IsMatch (System.Convert.ToString(value), @"[0-9]+.[0-9]+")
-                then this.EnginePower <- value
-                else System.Console.WriteLine("Invalid input")
-
-        override this.ToString() =            
-            sprintf "ID = %i\nName = %s\nModel = %s\nCategory = %s\nYearOfManufacture = %i\nEnginePower = %f\n"
-                this.ID this.Name this.Model this.Category this.YearOfManufacture this.EnginePower
-
-        interface IPrint with
-            member this.Print() =
-                this.ToString()
+@@ -59,7 +60,33 @@ type VehiclePassport(i:int, name:string, model:string, category:string, yearOfMa
                 |> printfn "%s\n"
         member this.Print() = (this :> IPrint).Print()
+
+        interface System.IComparable<VehiclePassport> with
+            member this.CompareTo other =
+                compare this.ID other.ID
+
+        interface System.IComparable with
+            member this.CompareTo object = 
+                match object with
+                |null -> 1
+                |   :? VehiclePassport as other -> (this :> System.IComparable<_>).CompareTo other
+                |_ -> invalidArg "object" "not a VehiclePassport"
+
+        interface System.IEquatable<VehiclePassport> with
+            member this.Equals other =
+                (this.ID = other.ID && this.YearOfManufacture = other.YearOfManufacture)
+
+        override this.Equals object = 
+            match object with
+            | :? VehiclePassport as other -> (this :> System.IEquatable<_>).Equals other
+            |_ -> false
+        override this.GetHashCode () = hash this.ID
 
 let ForDemo =
     let rand = System.Random()
     let vehicles = List.init(10) (fun v -> VehiclePassport((rand.Next(100, 1000)), "unnamed", "unknown", "undefined", (rand.Next(2000, 2022)), 0.0001))
     vehicles |> List.iter (fun v -> v.Print())  
+    //vehicles |> List.iter (fun v -> v.Print())  
+    //Equation by ID and YearOfManufacture
+    //Comparison by ID
+    printfn "%A" ((new VehiclePassport(1, "", "", "", 10, 0.0)).Equals(new VehiclePassport(2, "", "", "", 10, 0.4))) // false
+    printfn "%A" ((new VehiclePassport(1, "", "", "", 10, 0.0)).Equals(new VehiclePassport(1, "", "", "", 10, 0.4))) // true
+    printfn "%A" (compare (new VehiclePassport(10, "", "", "", 20, 0.0)) (new VehiclePassport(4, "", "", "", 40, 0.4)))// 1
